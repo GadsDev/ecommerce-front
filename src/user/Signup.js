75fcb333ce057aa86df693a4cc56c39374d2680a
gustavo.silva <gustavo.silva@codificar.com.br>
2020-08-17 20:46:39 -0300

    Signup Form

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import Layout from "../core/Layout"
-import {API} from "../../config"
 
-const Signup = () => (
-    <Layout title="Signup" description="Node React E-commerce App">
-        {API}
-    </Layout>
-)
+const Signup = () => {
+    const signUpForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-mute">Name</label>
+                <input type="text" className="form-control"/>
+            </div>
+            <div className="form-group">
+                <label className="text-mute">Email</label>
+                <input type="email" className="form-control"/>
+            </div>
+            <div className="form-group">
+                <label className="text-mute">Password</label>
+                <input type="password" className="form-control"/>
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    )
+    return (
+        <Layout title="Signup" description="Node React E-commerce App" className="container col-md-8 offset-md-2">
+            {signUpForm()}
+        </Layout>
+    )    
+}
 
 export default Signup
