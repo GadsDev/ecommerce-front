e39f97568a668bb6f3e963889973b8ef6ee126c1
gustavo.silva <gustavo.silva@codificar.com.br>
2020-08-16 22:20:29 -0300

    Home Route

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Signup from './user/Signup'
 import Signin from './user/Signin'
+import Home from './core/Home'
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/signin" component={Signin} />
-                <Route exact path="/signup" component={Signup}/>
+                <Home exact path="/" component={Home}/>
+                <Route exact path="/signin" component={Signin} />
+                <Route exact path="/signup" component={Signup} />                
             </Switch>
         </BrowserRouter>
     )
