a675c3422f124103015771140193bfa743a026a8
gustavo.silva <gustavo.silva@codificar.com.br>
2020-08-17 21:14:10 -0300

    User signup with  fetch

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,41 +1,82 @@
-import React, { useState } from 'react'
-import Layout from "../core/Layout"
+import React, { useState } from "react";
+import Layout from "../core/Layout";
+import { API } from "../config";
 
 const Signup = () => {
-    const [values, setValues] = useState({
-        name: '',
-        email: '',
-        password: '',
-        error: '',
-        sucess: false
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    sucess: false,
+  });
+  const { name, email, password } = values;
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const signup = (user) => {
+    fetch(`${API}/signup`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
     })
+      .then((res) => {
+        console.log("SUCCESS", res);
+      })
+      .catch((err) => {
+        console.log("ERROR", err);
+      });
+  };
 
-    const handleChange = name => event => {
-        setValues({...values, error: false, [name] : event.target.value})
-    } 
-    const signUpForm = () => (
-        <form>
-            <div className="form-group">
-                <label className="text-mute">Name</label>
-                <input onChange={handleChange('name')} type="text" className="form-control"/>
-            </div>
-            <div className="form-group">
-                <label className="text-mute">Email</label>
-                <input onChange={handleChange('email')} type="email" className="form-control"/>
-            </div>
-            <div className="form-group">
-                <label className="text-mute">Password</label>
-                <input onChange={handleChange('password')} type="password" className="form-control"/>
-            </div>
-            <button className="btn btn-primary">Submit</button>
-        </form>
-    )
-    return (
-        <Layout title="Signup" description="Node React E-commerce App" className="container col-md-8 offset-md-2">
-            {signUpForm()}
-            {JSON.stringify(values)}
-        </Layout>
-    )    
-}
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    signup({ name, email, password });
+  };
+  const signUpForm = () => (
+    <form>
+      <div className="form-group">
+        <label className="text-mute">Name</label>
+        <input
+          onChange={handleChange("name")}
+          type="text"
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-mute">Email</label>
+        <input
+          onChange={handleChange("email")}
+          type="email"
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-mute">Password</label>
+        <input
+          onChange={handleChange("password")}
+          type="password"
+          className="form-control"
+        />
+      </div>
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+  return (
+    <Layout
+      title="Signup"
+      description="Node React E-commerce App"
+      className="container col-md-8 offset-md-2"
+    >
+      {signUpForm()}
+    </Layout>
+  );
+};
 
-export default Signup
+export default Signup;
