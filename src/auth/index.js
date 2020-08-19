7f7e77573b9c083cd066f4ef04cb893be846d4e7
gustavo.silva <gustavo.silva@codificar.com.br>
2020-08-18 21:40:32 -0300

    Signout

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,42 +1,58 @@
 import { API } from "../config";
 
 export const signup = (user) => {
-    return fetch(`${API}/signup`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
+  return fetch(`${API}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
     })
-      .then((response) => {
-        return response.json();
-      })
-        .catch((err) => {
-            console.log("signup err", err);
-      });
+    .catch((err) => {
+      console.log("signup err", err);
+    });
 };
 
 export const signin = (user) => {
-    return fetch(`${API}/signin`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
+  return fetch(`${API}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
     })
-      .then((response) => {
-        return response.json();
-      })
-        .catch((err) => {
-          console.log("signin err", err);
-      });
+    .catch((err) => {
+      console.log("signin err", err);
+    });
 };
 
 export const authenticate = (data, next) => {
-    if (typeof window !== "undefined") {
-        localStorage.setItem('jwt', JSON.stringify(data))
-        next()
-    }
-}
+  if (typeof window !== "undefined") {
+    localStorage.setItem("jwt", JSON.stringify(data));
+    next();
+  }
+};
+
+export const signout = next => {
+  if (typeof window !== "undefined") {
+    localStorage.removeItem("jwt");
+    next();
+    return fetch(`${API}/signout`, {
+      method: "GET",
+    })
+      .then((response) => {
+        console.log("Signout", response);
+      })
+      .catch((err) => {
+        console.log("signout err", err);
+      });
+  }
+};
--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,30 +1,58 @@
-import React from 'react'
-import { Link, withRouter } from 'react-router-dom'
-
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import { signout } from "../auth";
 const isActive = (history, path) => {
-    if(history.location.pathname === path) {
-        return {color: '#ff9000'}
-    } else {
-        return { color: '#ffff' }
-    }
-}
+  if (history.location.pathname === path) {
+    return { color: "#ff9000" };
+  } else {
+    return { color: "#ffff" };
+  }
+};
 
-const Menu = (history) => (
-    <div>
-        <ul className="nav nav-tabs bg-primary">
-            <li className="nav-item">
-                <Link style={isActive(history, '/')} className="nav-link" to="/">Home</Link>
-            </li>
+const Menu = ({history}) => (
+  <div>
+    <ul className="nav nav-tabs bg-primary">
+      <li className="nav-item">
+        <Link style={isActive(history, "/")} className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
 
-            <li className="nav-item">
-                <Link style={isActive(history, '/signin')} className="nav-link" to="/signin">Signin</Link>
-            </li>
+      <li className="nav-item">
+        <Link
+          style={isActive(history, "/signin")}
+          className="nav-link"
+          to="/signin"
+        >
+          Signin
+        </Link>
+      </li>
 
-            <li className="nav-item">
-                <Link style={isActive(history, '/signup')} className="nav-link" to="/signup">Signup</Link>
-            </li>
-        </ul>
-    </div>
-)
+      <li className="nav-item">
+        <Link
+          style={isActive(history, "/signup")}
+          className="nav-link"
+          to="/signup"
+        >
+          Signup
+        </Link>
+      </li>
 
-export default withRouter(Menu)
+      <li className="nav-item">
+        <span
+          style={{ cursor: "pointer", color: "#ffffff" }}
+          className="nav-link"
+          onClick={() =>
+            signout(() => {
+              history.push("/")
+            })
+          }
+        >
+          Signout
+        </span>
+      </li>
+    </ul>
+  </div>
+);
+
+export default withRouter(Menu);
