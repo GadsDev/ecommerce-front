5d0357be77d2c97329ff8324d9c30f250b24bd16
gustavo.silva <gustavo.silva@codificar.com.br>
2020-09-08 21:14:21 -0300

    Style and add details

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,31 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ShowImage from "./ShowImage";
+import moment from "moment";
 
 const Card = ({ product, showViewProductButton = true }) => {
-  const showViewButton = showViewProductButton => {
+  const showViewButton = (showViewProductButton) => {
     return (
       showViewProductButton && (
-        <button className="btn btn-outline-primary mr-2 mb-2">
-          View Product
-        </button>
+        <Link to={`/product/${product._id}`}>
+          <button className="btn btn-outline-primary mr-2 mb-2">
+            View Product
+          </button>
+        </Link>
       )
-    )
-  }
+    );
+  };
+
+  const showAddToCartButton = () => {
+    return (
+      <button className="btn btn-outline-warning mr-2 mb-2">Add to card</button>
+    );
+  };
+
+  const showStock = (quantity) => {
+    return quantity > 0 ? (
+      <span className="mb-2 badge badge-primary badge-pill">In Stock</span>
+    ) : (
+      <span className="mb-2 badge badge-primary badge-pill">Out of Stock</span>
+    );
+  };
 
   return (
     <div className="card">
-      <div className="card-header">{product.name}</div>
+      <div className="card-header name">{product.name}</div>
       <div className="card-body">
         <ShowImage item={product} url="product" />
-        <p>{product.description}</p>
-        <p>$ {product.price}</p>
-        <Link to={`/product/${product._id}`}> 
-          {showViewButton(showViewProductButton)}         
-        </Link>
-        <button className="btn btn-outline-warning mr-2 mb-2">
-          Add to card
-        </button>
+        <p className="lead mt-2">{product.description.substring(0, 100)}</p>
+        <p className="black-10">${product.price}</p>
+        <p className="black-9">
+          Category: {product.category && product.category.name}
+        </p>
+
+        <p className="black-8">Added on {moment(product.createdAt).fromNow()}</p>
+
+        {showStock(product.quantity)}
+        <br/>
+
+        {showViewButton(showViewProductButton)}
+
+        {showAddToCartButton()}
       </div>
     </div>
   );
